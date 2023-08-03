198f9745d127f37a05a897d5a8b2b941783d78ca
EvgeniiIudin <113230787+EvgeniiIudin@users.noreply.github.com>
2023-08-03 19:09:32 +0300

    Пятая домашняя работа

---
+++ homework5_1.py
@@ -0,0 +1,15 @@
+"""Напишите функцию, которая принимает на вход строку —
+абсолютный путь до файла. Функция возвращает кортеж из трёх
+элементов: путь, имя файла, расширение файла."""
+
+import os
+
+string = "C:\miste\PycharmProjects\pythonProject\homework5_1.py"
+
+def fun(f_path: str) -> tuple:
+    path, filename = os.path.split(f_path)
+    name, extension = filename.split('.')
+    return path, name, extension
+
+print(f'Исходная строка: {string}')
+print(f'Кортеж из пути: {fun(string)}')
